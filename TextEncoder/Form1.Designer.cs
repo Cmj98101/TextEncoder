7f5072f4f50a059991b3fe62146dfcfa36f73b08
christopher Jackson Desktop <christopher.jtech@gmail.com>
2020-08-05 20:28:03 -0700

    Add project files.

---
+++ TextEncoder/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace TextEncoder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.fileNameHeader = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.openFilesBtn = new System.Windows.Forms.Button();
+            this.convertBtn = new System.Windows.Forms.Button();
+            this.deleteBtn = new System.Windows.Forms.Button();
+            this.deleteAllBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.fileNameHeader});
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(12, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(594, 203);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // fileNameHeader
+            // 
+            this.fileNameHeader.Text = "File Name";
+            this.fileNameHeader.Width = 594;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "DXF Files (*.dxf)|*.dxf|All Files (*.*)|*.*";
+            this.openFileDialog1.Multiselect = true;
+            // 
+            // openFilesBtn
+            // 
+            this.openFilesBtn.Location = new System.Drawing.Point(12, 232);
+            this.openFilesBtn.Name = "openFilesBtn";
+            this.openFilesBtn.Size = new System.Drawing.Size(594, 30);
+            this.openFilesBtn.TabIndex = 1;
+            this.openFilesBtn.Text = "Open";
+            this.openFilesBtn.UseVisualStyleBackColor = true;
+            this.openFilesBtn.Click += new System.EventHandler(this.openFilesBtn_Click);
+            // 
+            // convertBtn
+            // 
+            this.convertBtn.Location = new System.Drawing.Point(12, 268);
+            this.convertBtn.Name = "convertBtn";
+            this.convertBtn.Size = new System.Drawing.Size(594, 30);
+            this.convertBtn.TabIndex = 2;
+            this.convertBtn.Text = "Convert";
+            this.convertBtn.UseVisualStyleBackColor = true;
+            this.convertBtn.Click += new System.EventHandler(this.convertBtn_Click);
+            // 
+            // deleteBtn
+            // 
+            this.deleteBtn.Location = new System.Drawing.Point(12, 304);
+            this.deleteBtn.Name = "deleteBtn";
+            this.deleteBtn.Size = new System.Drawing.Size(594, 30);
+            this.deleteBtn.TabIndex = 3;
+            this.deleteBtn.Text = "Delete";
+            this.deleteBtn.UseVisualStyleBackColor = true;
+            this.deleteBtn.Click += new System.EventHandler(this.deleteBtn_Click);
+            // 
+            // deleteAllBtn
+            // 
+            this.deleteAllBtn.Location = new System.Drawing.Point(12, 340);
+            this.deleteAllBtn.Name = "deleteAllBtn";
+            this.deleteAllBtn.Size = new System.Drawing.Size(594, 30);
+            this.deleteAllBtn.TabIndex = 4;
+            this.deleteAllBtn.Text = "Delete All";
+            this.deleteAllBtn.UseVisualStyleBackColor = true;
+            this.deleteAllBtn.Click += new System.EventHandler(this.deleteAllBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(618, 377);
+            this.Controls.Add(this.deleteAllBtn);
+            this.Controls.Add(this.deleteBtn);
+            this.Controls.Add(this.convertBtn);
+            this.Controls.Add(this.openFilesBtn);
+            this.Controls.Add(this.listView1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(634, 416);
+            this.MinimumSize = new System.Drawing.Size(634, 416);
+            this.Name = "Form1";
+            this.Text = "Text Encoder";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button openFilesBtn;
+        private System.Windows.Forms.Button convertBtn;
+        private System.Windows.Forms.Button deleteBtn;
+        private System.Windows.Forms.Button deleteAllBtn;
+        private System.Windows.Forms.ColumnHeader fileNameHeader;
+    }
+}
+
---
+++ TextEncoder/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TextEncoder
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void CreatePath(String dirPath)
+        {
+            if (Directory.Exists(dirPath))
+            {
+                Console.WriteLine($"Directory Exists: {dirPath}");
+                return;
+            }
+            else
+            {
+                try
+                {
+                    Directory.CreateDirectory(dirPath);
+                    
+                }
+                catch (Exception error)
+                {
+                    Console.WriteLine($"Create Path Error:{error}");
+                    MessageBox.Show($"Create Path Error:{error}");
+                    return;
+                }
+            }
+
+        }
+        private void openFilesBtn_Click(object sender, EventArgs e)
+        {
+            
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                System.IO.Stream myStream;
+                string fileName;
+                
+                fileName = openFileDialog1.FileName;
+                foreach (String filePath in openFileDialog1.FileNames)
+                {
+                    try
+                    {
+                        if ((myStream = openFileDialog1.OpenFile()) != null)
+                        {
+                            using (myStream)
+                            {
+                                Console.WriteLine($"{Path.GetFileName(filePath)}");
+                                listView1.Items.Add($"{Path.GetFileName(filePath)}").SubItems.Add($"{filePath}");
+                            }
+                        }
+                    }
+
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                    }
+                }       
+            }
+        }
+        private void convertBtn_Click(object sender, EventArgs e)
+        {
+
+            // 1.) Grab all Items in the List
+            foreach (ListViewItem item in listView1.Items)
+            {
+                
+                //Console.WriteLine($"{item.SubItems[1].Text}");
+                String encodedFile;
+                String newDir = $"{Path.GetDirectoryName($"{item.SubItems[1].Text}")}/Encoded DXF";
+                String newPath = $"{Path.GetDirectoryName($"{item.SubItems[1].Text}")}/Encoded DXF/{Path.GetFileName(item.SubItems[1].Text)}";
+
+                // 2.) Foreach Item Grab the Path and read each file to the end 
+                using (StreamReader sr = new StreamReader($"{item.SubItems[1].Text}"))
+                {
+                    String fileContents = sr.ReadToEnd();
+                    // (?=^|\r?\n)\bTEXT\b
+                    // Regex Pattern
+                    String pattern1 = @"(?=^|\r?\n)\bTEXT\b";
+                    String pattern2 = @"(?=^|\r?\n)\bAcDbText\b";
+                    // Replacement Text
+                    String replacement1 = "MTEXT";
+                    String replacement2 = "AcDbMText";
+
+                    // Multiline option to read not only the first line
+                    RegexOptions options = RegexOptions.Multiline;
+
+
+                    Regex rgx1 = new Regex(pattern1, options);
+                    Regex rgx2 = new Regex(pattern2, options);
+
+                    // Looks for "TEXT" && "AcDbText" and Replaces it with "MTEXT" && "AcDbMText"
+                    encodedFile = rgx1.Replace(fileContents, replacement1);
+                    encodedFile = rgx2.Replace(encodedFile, replacement2);
+
+
+                    // Prints out new Encoded String
+                    Console.WriteLine($"{encodedFile}");
+
+                }
+
+                // 3.) Create Directory if it is not already created
+                CreatePath(newDir);
+                // 4.) Write Encoded String to new File inside of file directory (Encoded DXF/Encoded File)
+                using (StreamWriter sw = new StreamWriter(newPath))
+                {
+                    sw.WriteLine($"{encodedFile}");
+                }
+
+                // 5.) Remove File from the List
+                item.Remove();
+            }
+        }
+
+        private void deleteBtn_Click(object sender, EventArgs e)
+        {
+            ListView.SelectedListViewItemCollection list = listView1.SelectedItems;
+
+            foreach (ListViewItem item in list)
+            {
+                item.Remove();
+            }
+        }
+
+        private void deleteAllBtn_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+        }
+    }
+}
